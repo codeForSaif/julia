976a0600c5697085a30bad840c971d8a80cfbf6c
Stefan Karpinski <stefan@karpinski.org>
2015-12-12 12:42:09 -0500

    highlander: rename UTF8String, ByteString => String

--- test/docs.jl
+++ test/docs.jl
@@ -488,7 +488,7 @@ type C <: A end
 
 immutable D <: B
     one
-    two::UTF8String
+    two::String
     three::Float64
 end
 
@@ -555,7 +555,7 @@ immutable Undocumented.D <: Undocumented.B
 **Fields:**
 ```julia
 one   :: Any
-two   :: UTF8String
+two   :: String
 three :: Float64
 ```
 """)
